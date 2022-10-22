484340d0bab7fe51110d628b4468697fef2fcbb5
sanyuyuyu <1733659500@qq.com>
2022-10-23 00:16:50 +0800

    java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,13 @@
-public class Hello 
+// 时间： 2022年10月12日
+// 地点：浙江邮电职业技术学院信通楼
+
+
+
+class Test_variable
 {
-	public static void main(String[] args) {
-		System.out.println("Hello World.");		
-	}
+	public static void main(String[] args)
+	 {
+		System.out.println("hello abc");		
+	 }
 
 }
