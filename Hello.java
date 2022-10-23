362992e009208a8959ab08d2772046207a0c4762
sanyuyuyu <1733659500@qq.com>
2022-10-23 15:26:25 +0800

    20221023

--- Hello.java
+++ Hello.java
@@ -2,12 +2,12 @@
 // 地点：浙江邮电职业技术学院信通楼
 
 
-
+ 
 class Test_variable
 {
 	public static void main(String[] args)
 	 {
-		System.out.println("hello abc");		
-	 }
-
+	
+		System.out.println("hello");	
+     }    
 }
---
+++ Test_variable.java
@@ -0,0 +1,42 @@
+// 时间： 2022年10月12日
+// 地点：浙江邮电职业技术学院信通楼
+/* 目的：用于学习java
+*/
+
+
+ 
+class Test_variable
+{
+	public static void main(String[] args)
+	 {
+		int a = 2147483647;
+		System.out.println("a的值为:"+(a+1));	
+		float b =  3.4028235E38f;
+		System.out.println("b的值为:"+(b+1));	
+		//long c = (long)a+1;
+		//System.out.println("c的值为:"+c);
+		double d = (double)b+1;
+		System.out.println("d的值为:"+d);
+		char c = 'C';	
+		long e = (long)a+c;
+		System.out.println("c的值为:"+c);
+        System.out.println("a+c的值为:"+e);
+		String s = "我是一个字符串变量";
+		System.out.println("s的值为:"+s);
+		int aa = (((20+30)-1)*2)/3;
+		System.out.println("aa的值为:"+aa);
+		int bb = 10 % 3;
+		System.out.println("bb的值为:"+bb);
+        boolean cc = 3 > 3.1;
+		System.out.println("cc的值为:"+cc);
+		boolean dd = 3 > 'A';
+		System.out.println("dd的值为:"+dd);
+		int x = 100;
+		int y = 50;
+		int max = 0; max = x > y ? x : y;
+		System.out.println("max的值为:"+max);
+		boolean ee = (1 > 2) || (10 / 2 == 0 );
+		System.out.println("ee的值为:"+ee);
+
+     }    
+}
