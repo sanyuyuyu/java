443c8f526925653d61b46ea6bb051df7e89451fb
sanyuyuyu <1733659500@qq.com>
2022-10-27 19:09:41 +0800

    20221027

--- Test_cho.java
+++ Test_cho.java
@@ -2,15 +2,28 @@
 // 地点：浙江邮电职业技术学院信通楼
 
 
- 
+
 class Test_cho
 {
-	public static void main(String[] args)
-	 {
-	    boolean rain = true;
-		if(rain==true);
-		System.out.println("今天下雨了，记得带伞");	
+	  public static void main(String[] args)
+	    {
+	    	
+	    	for(int x = 1;x<=9;x++)
+	    	{
+	    	//	System.out.println("第"+i+"行乘法：");
+
+	    		for(int y = 1;y <= x;y++)
+	    		{
+	    				System.out.print(y+"*"+x+"="+y*x);
+							System.out.print("\t");
+	    		}
+	  			
+	     	  	System.out.print("\n");
+
+	    	}
+	    	
+
+	    }
 	
 	
-     }    
 }
---
+++ Test_method.java
@@ -0,0 +1,18 @@
+class Test_method
+{ 
+    public static void main(String[] args)
+    {
+        getprintname();
+        getprintname();
+
+    }
+        public static void getprintname()
+        { 
+            String a = "your name is so handsome";
+            for(int x = 1; x < 4; x++)
+
+            System.out.println(a);
+
+        }
+
+}
