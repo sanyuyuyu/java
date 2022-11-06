f10d50a0e02a909a99fb766d57f8bb51c9e1356a
sanyuyuyu <1733659500@qq.com>
2022-11-06 20:44:02 +0800

    20221106

---
+++ Test_four.java
@@ -0,0 +1,51 @@
+/*class Test_four
+{
+    public static void main(String[] args)
+    {
+      Person ming = new Person();
+      ming.setBirth(2022);
+      System.out.println(ming.getAgr());
+
+
+    }
+
+}
+class Person
+{
+    private String name;
+    private int birth;
+    
+    public void setBirth(int birth)
+    {
+        this.birth = birth;
+    }
+    
+    public int getAge()
+    {
+        return calcAge(2022);
+    }
+
+     private int calcAge(int currentYear);
+     {
+        return currentYear - this.birth;
+     }
+}
+*/
+
+
+class Test_four
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public static int getIntSum(int i , int j)
+    {
+        for(; j<i;i++)
+        {
+            break;
+        }
+        return;
+    }
+}
