72a3309d76d034a882235264ca807aee717da11f
sanyuyuyu <1733659500@qq.com>
2022-11-07 23:24:23 +0800

    20221107

---
+++ Test_five.java
@@ -0,0 +1,102 @@
+public class Test_five
+{
+     public static void main(String[] args)
+  {
+
+     String fruit = "apple";
+     switch (fruit)
+   {
+    case "apple":
+    System.out.println("i like apple");
+    break; //switch 中break不能省略
+   }
+    /*int option = 99;
+        switch (option) 
+    {
+        case 1:  // 相当于if语句
+            System.out.println("Selected 1");
+            break;
+        case 2:
+            System.out.println("Selected 2");
+            break;
+        case 3:
+            System.out.println("Selected 3");
+            break;
+        default:
+            System.out.println("Not selected");
+            break;
+            */
+   }
+  }
+
+
+   /* int n = 33;
+    if (n<=45)
+    {
+        System.out.println("your's achievement: "+n);
+    }
+    else
+    {
+        System.out.println("yes");
+    }
+
+        String s1 = "hello";
+        String s2 = "HELLO".toLowerCase();
+        System.out.println(s1);
+        System.out.println(s2);
+        if (s1.equals(s2)) {
+            System.out.println("s1 equals s2");
+        } else {
+            System.out.println("s1 not equals s2");
+        }
+*/
+
+
+      /*  int option = 1;
+        switch (option)
+        {
+            case 1:
+            System.out.println("Selected 1");
+            break;
+            case 2:
+            System.out.println("Selected 2");
+            break;
+            case 3:
+            System.out.println("Selected 3");
+            break;
+
+        }
+*/
+    /*    int option = 99;
+        switch (option)
+        {
+            case 1:
+            System.out.ptintln("Selected 1");
+            break;
+            case 2:
+            System.out.ptintln("Selected 2");
+            break;
+            case 3:
+            System.out.println("Selected 3");
+            break;
+            default:
+            System.out.ptintln("Not in Selected");
+            break;
+        }
+    
+  }*/
+   /*int p4,p3,p2,p1;
+   int n = 1000;
+    while (n <= 9999)
+    { 
+        p1 = n % 10;
+        p2 = (n - p1) / 10 % 10;
+        p3 = (n - 10 * p2 - p1) / 100 % 10;
+        p4 = (n - 10 * p3 - 10 * p2 - p1) / 1000;
+
+        if (n ==p4 * p4 * p4 * p4+ p3 * p3 * p3 * p3 + p2 * p2 * p2 * p2 + p1 * p1 * p1 * p1){
+            System.out.println(n + "=" +p4 + "^4 +" + p3 + "^4 +"+ p2 + "^4 +"+ p1 + "^4");
+        }
+        n++;
+    }*/
+//}
--- Test_four.java
+++ Test_four.java
@@ -37,15 +37,26 @@ class Test_four
 {
     public static void main(String[] args)
     {
-
+        int i = 10;
+        int j = 1;
+        int total = getIntSum(i, j);
+        System.out.println(total);
     }
 
     public static int getIntSum(int i , int j)
     {
-        for(; j<i;i++)
+        int sum = 0;
+        for(; j<i;j++)
         {
-            break;
+            // System.out.println(j);
+            System.out.println(i+","+j);
+            sum = sum + j;
+            // if (j == 4)
+            // {
+            //     continue;
+            // }      
+            // System.out.println("abcde");
         }
-        return;
+        return sum;
     }
 }
