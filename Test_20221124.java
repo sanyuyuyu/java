a9289a9f4c1431d7165a7eef7ee8fbe4a9fee059
sanyuyuyu <1733659500@qq.com>
2022-11-24 17:34:05 +0800

    20221124

---
+++ Test_20221124.java
@@ -0,0 +1,56 @@
+/*class Test_20221124
+{
+    public static void main(String[] args)
+    {
+        int sum = 0; //定义sum的初始值
+        for(int x = 1; x <= 100;x++) //循环1到99
+        {
+            if(x%2!=0) //为奇数则累加到sum中
+            {
+                sum += x;
+            }
+        }
+        System.out.println(sum); //输出累加sum的和
+    }
+}*
+/*
+class Test_20221124
+{
+    public static void main(String[] args)
+    {
+
+        /*int a = 3;
+        int b = 2;
+
+        switch(b)
+        {
+            case 1:
+            a--;
+            break;
+            case 2:
+            a++;
+            case 3:
+            a = a + 3;
+            default:
+            a++;
+            break;
+        }
+        System.out.println("a="+a);
+*//*
+        int x = 2;
+        
+        int max = x>0? x+1:5;
+        System.out.println(max);*/
+    // }
+//}*/
+
+
+class Test_20221124
+{
+    public static void main(String[] args)
+    {
+        int[] temp1 = new int[]{22,24,76,12,21,33};
+        
+        
+    }
+}
