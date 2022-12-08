8dbb91f1b6e9bb1add0fc156f0587fa5b649d1aa
sanyuyuyu <1733659500@qq.com>
2022-12-08 17:41:01 +0800

    20221208

--- Test_20221208.java
+++ Test_20221208.java
@@ -1,18 +1,19 @@
+
+System.arraycopy(arr1,0,arr2,0,m);
 class Test_20221208
 {
     public static void main(String[] args)
     {
+            // int[] arr1 = {11,22,33,44,55};
+            // int[] arr2 = {1,2,3,4,5,6,7};
+            // System.out.println("替换后的arr1"+arraycopy(arr1,2,arr2,4,3));
+    //     int[] arr1 ={1,2,3,4,5};
+    //    //arrayCopy(arr1, 3, arr1, 2, 2);
+    //     System.out.println(arrayCopy(arr1, 3, arr1, 2, 2));
 
-    }
-}
-
-class Stu
-{
-    public String name;
-    public int age;
-
-    public Stu()
-    {
-        
+            int[] arr1 = {11,22,33,44,55};
+            int[] arr2 = {1,2,3,4,5,6,7};
+            System.arraycopy(arr1,2,arr1,2,3);
+            System.out.println(Arrays.toString(arr2));
     }
 }
