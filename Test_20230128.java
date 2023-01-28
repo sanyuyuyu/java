28f765dc611d8a87cf91aa3d05d5601186bc9a2d
sanyuyuyu <1733659500@qq.com>
2023-01-28 23:51:15 +0800

    20230128

---
+++ Test_20230128.java
@@ -0,0 +1,10 @@
+class Test_20230128
+{
+    public static void main(String[] args)
+    {
+        int score = 99;
+        p.setScore(score);
+        log.info("Set score" + score + "for Person "+p.getName() + "ok .");
+        
+    }
+}
