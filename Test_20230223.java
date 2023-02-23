66d537d61d378e3f462f0d6dd0d9b1cac4e2903f
sanyuyuyu <1733659500@qq.com>
2023-02-23 22:28:15 +0800

    20230222

---
+++ Test_20230223.java
@@ -0,0 +1,12 @@
+class Test_20230223
+{
+    public static void main(String[] args)
+    {
+        int a = 2;
+        int b = (a++) * 3;
+        int c = (++a) * 3;
+        System.out.println(b);
+
+        
+    }
+}
